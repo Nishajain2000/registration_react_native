730664d93cdb596ccdb2eacca4aaa58c30687765
Nishajain2000 <Nishajain7040@gmail.com>
2019-08-26 12:33:26 +0530

    react native file

---
+++ jain/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {WebView} from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView source={{uri:'http:/192.168.0.139/register/index.php'}}/>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 5,
+    backgroundColor: '#ce93d8',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+
+  },
+});
